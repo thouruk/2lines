acd9b2328b5f6d1113b40ee29d311b0433d7c287
xol <xol574@gmail.com>
2023-05-15 21:07:21 +0200

    wersja 1.0

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # 2lines
+Program narysuje przedstawione proste, sprawdzi i poda punkty przeciecia
--- main.py
+++ main.py
@@ -1,3 +1,7 @@
+import tkinter as tk
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+
 
 def orientation(p, q, r):
     # Funkcja pomocnicza do określenia orientacji punktów p, q, r
@@ -6,9 +10,11 @@ def orientation(p, q, r):
     # wartość zero, jeśli punkty są współliniowe.
     return (q[1] - p[1]) * (r[0] - q[0]) - (q[0] - p[0]) * (r[1] - q[1])
 
+
 def on_segment(p, q, r):
     # Funkcja pomocnicza sprawdzająca, czy punkt q leży na odcinku p-r
     return min(p[0], r[0]) <= q[0] <= max(p[0], r[0]) and min(p[1], r[1]) <= q[1] <= max(p[1], r[1])
+
 
 def intersect(p1, q1, p2, q2):
     # Funkcja główna wyznaczająca przecięcie dwóch odcinków
@@ -20,7 +26,7 @@ def intersect(p1, q1, p2, q2):
     # Sprawdzanie przypadków, w których odcinki się przecinają
     if o1 != o2 and o3 != o4:
         return True, (p1, q1) if on_segment(p1, p2, q1) else (p1, q2)
-    
+
     # Przypadki szczególne dla współliniowych odcinków
     if o1 == 0 and on_segment(p1, p2, q1):
         return True, (p1, q1)
@@ -33,21 +39,106 @@ def intersect(p1, q1, p2, q2):
 
     return False, None
 
-# Przykładowe dane wejściowe
-p1 = (1, 1)
-q1 = (4, 4)
-p2 = (1, 4)
-q2 = (4, 1)
 
-# Wywołanie funkcji intersect i wyświetlenie wyników
-intersecting, intersection = intersect(p1, q1, p2, q2)
+def check_intersection():
+    # Pobieranie współrzędnych odcinków z pól tekstowych
+    p1_x = float(entry_p1_x.get())
+    p1_y = float(entry_p1_y.get())
+    q1_x = float(entry_q1_x.get())
+    q1_y = float(entry_q1_y.get())
+    p2_x = float(entry_p2_x.get())
+    p2_y = float(entry_p2_y.get())
+    q2_x = float(entry_q2_x.get())
+    q2_y = float(entry_q2_y.get())
 
-if intersecting:
-    print("Odcinki przecinają się.")
-    if isinstance(intersection, tuple):
-        print("Przecięcie to odcinek o końcach:", intersection)
+    # Wywołanie funkcji intersect i wyświetlenie wyników
+    intersecting, intersection = intersect((p1_x, p1_y), (q1_x, q1_y), (p2_x, p2_y), (q2_x, q2_y))
+
+    if intersecting:
+        result_label.config(text="Odcinki przecinają się.")
+        if isinstance(intersection, tuple):
+            intersection_label.config(text="Przecięcie to odcinek o końcach: {}".format(intersection))
+        else:
+            intersection_label.config(text="Przecięcie to punkt o współrzędnych: {}".format(intersection))
+
+        # Wykres z odcinkami
+        fig, ax = plt.subplots()
+        ax.plot([p1_x, q1_x], [p1_y, q1_y], 'b', label='Odcinek 1')
+        ax.plot([p2_x, q2_x], [p2_y, q2_y], 'g', label='Odcinek 2')
+
+        # Dodanie układu współrzędnych
+        ax.axhline(0, color='black', linewidth=0.5)
+        ax.axvline(0, color='black', linewidth=0.5)
+
+        # Skala osi
+        ax.set_aspect('equal', adjustable='box')
+
+        # Legenda
+        ax.legend()
+
+        canvas = FigureCanvasTkAgg(fig, master=graph_frame)
+        canvas.draw()
+        canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        graph_frame.pack()
     else:
-        print("Przecięcie to punkt o współrzędnych:", intersection)
+        result_label.config(text="Odcinki nie przecinają się.")
+        intersection_label.config(text="")
+        graph_frame.pack_forget()  # Ukrycie wykresu
 
-else:
-    print("Odcinki nie przecinają się.")
+
+# Tworzenie interfejsu Tkinter
+root = tk.Tk()
+root.title("Przecięcie odcinków")
+
+# Ramka do wprowadzania danych
+input_frame = tk.Frame(root)
+input_frame.pack(pady=10)
+
+label_p1 = tk.Label(input_frame, text="Odcinek 1 - P:")
+label_p1.grid(row=0, column=0, padx=5)
+entry_p1_x = tk.Entry(input_frame, width=5)
+entry_p1_x.grid(row=0, column=1, padx=5)
+entry_p1_y = tk.Entry(input_frame, width=5)
+entry_p1_y.grid(row=0, column=2, padx=5)
+
+label_q1 = tk.Label(input_frame, text="Q:")
+label_q1.grid(row=0, column=3, padx=5)
+entry_q1_x = tk.Entry(input_frame, width=5)
+entry_q1_x.grid(row=0, column=4, padx=5)
+entry_q1_y = tk.Entry(input_frame, width=5)
+entry_q1_y.grid(row=0, column=5, padx=5)
+
+label_p2 = tk.Label(input_frame, text="Odcinek 2 - P:")
+label_p2.grid(row=1, column=0, padx=5)
+entry_p2_x = tk.Entry(input_frame, width=5)
+entry_p2_x.grid(row=1, column=1, padx=5)
+entry_p2_y = tk.Entry(input_frame, width=5)
+entry_p2_y.grid(row=1, column=2, padx=5)
+
+label_q2 = tk.Label(input_frame, text="Q:")
+label_q2.grid(row=1, column=3, padx=5)
+entry_q2_x = tk.Entry(input_frame, width=5)
+entry_q2_x.grid(row=1, column=4, padx=5)
+entry_q2_y = tk.Entry(input_frame, width=5)
+entry_q2_y.grid(row=1, column=5, padx=5)
+
+# Przycisk sprawdzający przecięcie
+check_button = tk.Button(root, text="Sprawdź przecięcie", command=check_intersection)
+check_button.pack(pady=10)
+
+# Ramka z wynikami
+result_frame = tk.Frame(root)
+result_frame.pack()
+
+result_label = tk.Label(result_frame, text="")
+result_label.pack()
+
+intersection_label = tk.Label(result_frame, text="")
+intersection_label.pack()
+
+# Ramka na wykres
+graph_frame = tk.Frame(root)
+
+root.mainloop()
+
